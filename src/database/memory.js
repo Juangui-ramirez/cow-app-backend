fcdd2c97ecebe2900d11843909a4ff858f74b47c
Juangui <juan-ramirez04@hotmail.com>
2024-04-01 18:31:11 -0500

    Adding DB

---
+++ src/database/memory.js
@@ -0,0 +1,27 @@
+//persistencia
+const petDB = [
+  {
+    name: "sorpresa",
+    color: "white",
+    weight: 1500,
+  },
+  {
+    name: "pequita",
+    color: "black",
+    weight: 2500,
+  },
+  {
+    name: "fresita",
+    color: "pink",
+    weight: 1500,
+  },
+];
+
+const groupDB = [
+  {
+    name: "Group 1",
+    color: "red",
+  },
+];
+
+module.exports = { petDB, groupDB};
