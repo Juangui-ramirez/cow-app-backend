91da05c303750c568f48766d894efb89160a1b3d
Juangui <juan-ramirez04@hotmail.com>
2024-07-04 13:13:56 -0500

    protect routes with auth

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,11 +5,21 @@ const pathsNoAuth = [
     path: "/auth/login",
     method: "POST",
   },
+  {
+    path: "/users",
+    method: "POST",
+  },
 ];
 
-const authenticateJWT = passport.authenticate("jwt", {
-  session: false,
-});
+const authenticateJWT = (req, res, next) => {
+  passport.authenticate("jwt", { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    req.user = user; // Establece el usuario autenticado en la solicitud
+    return next();
+  })(req, res, next);
+};
 
 const applyJWTAuthentication = (req, res, next) => {
   if (
